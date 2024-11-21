6e57a73d881dd2fd1923ea5704a11deefb02b114
wwsmms <mahmoodis2003@gmail.com>
2024-11-21 20:02:09 +0200

    add sign and digit_sum

--- hw1/ex1_214032682.c
+++ hw1/ex1_214032682.c
@@ -2,6 +2,22 @@
 int abs(int x){//calculate absulote value
     return (x<0) ? -x : x;
 }
+void sign(int num){//print sign
+    if (num>0)
+        printf("It’s sign is: +");
+    else if (num<0)
+        printf("It’s sign is: -");
+    else 
+        printf("It’s sign is: 0"); 
+}
+void digit_sum(int num){//should i add int org=num?
+    int sum=0;
+    while (num>0){
+        sum+=num%10;
+        num/=10;
+    }
+    printf("The sum of digits is:%d\n",sum);
+}
 int main(){
     printf("helloworld");
     return 0;
